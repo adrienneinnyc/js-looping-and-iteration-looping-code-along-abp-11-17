cc9e4ce16ecbbc7bd63162cc7a92344483eea06d
Adrienne Johnson <adriennetjohnson@gmail.com>
2017-11-18 18:44:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-const arr;
-
 function printBadges(names) {
-  for(let i = 0; i < names.length: i++) {
+  for(let i = 0; i < names.length; i++) {
     console.log(`Welcome ${names[i]}! You are employee #${i+1}.`)
   }
 }
 
-printBadges(arr);
